cdf9a1a1b5e3fc345e16f501ef4950c70af3a516
Sambit <sambit.basu.15@ucl.ac.uk>
2021-03-21 16:41:16 +0000

    crud established

---
+++ expenses/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from django.forms import ModelForm
+
+from .models import *
+
+
+class EntryForm(forms.ModelForm):
+    category = forms.CharField(widget= forms.TextInput(attrs={'placeholder':'Category'}))
+    amount = forms.CharField(widget= forms.TextInput(attrs={'placeholder':'Amount'}))
+    balance = forms.CharField(widget= forms.TextInput(attrs={'placeholder':'Balance'}))
+
+    class Meta:
+        model = EntryItem
+        fields = ('category', 'amount', 'balance', 'entry')
+        # fields = '__all__'
--- expenses/models.py
+++ expenses/models.py
@@ -12,22 +12,22 @@ from django.contrib.auth.models import User
 #         return self.name
 
 
-# class Account(models.Model):
-#     '''The account of the user, containing several EntryItem objects'''
-#     # user = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True)
-#     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
-#     name = models.CharField(max_length=200, null=True)
-#
-#     @property
-#     def total_spending(self):
-#         account_items = self.entryitem_set.all()
-#         total = sum([item.amount for item in account_items])
-#         return total
+class Account(models.Model):
+    '''The account of the user, containing several EntryItem objects'''
+    # user = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
+    name = models.CharField(max_length=200, null=True)
+
+    @property
+    def total_spending(self):
+        account_items = self.entryitem_set.all()
+        total = sum([item.amount for item in account_items])
+        return total
 
 
 class EntryItem(models.Model):
     '''The entry submitted to the Account by a User'''
-    date = models.DateTimeField()
+    date = models.DateField(auto_now_add = True)
     category = models.CharField(max_length=20, null=True)
     amount = models.FloatField()
     balance = models.FloatField()
